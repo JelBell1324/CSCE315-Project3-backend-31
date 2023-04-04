d968367d2602974df475d671b52c469e47814bbb
cherry13245 <cherrytree1324@tamu.edu>
2023-04-04 17:48:50 -0500

    added makeorder

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -2,6 +2,53 @@ import express from "express";
 import pool from "../server.js";
 const orderRouter = express.Router();
 
+orderRouter.post("/makeorder", async (req, res) => {
+	const { cost_total, timestamp, customer_id, staff_id, menu_items } =
+		req.body;
+
+	try {
+		const { rows } = await pool.query(
+			`INSERT INTO orders (cost_total, date, customer_id, staff_id)
+		 VALUES ($1, $2, $3, $4)
+		 RETURNING order_id;`,
+			[cost_total, timestamp, customer_id, staff_id]
+		);
+
+		const order_id = rows[0].order_id;
+
+		for (const menuItem of menu_items) {
+			await pool.query(
+				`INSERT INTO menu_to_order (menu_id, order_id, quantity)
+		   VALUES ($1, $2, $3);`,
+				[menuItem.menu_id, order_id, menuItem.quantity]
+			);
+		}
+
+		for (const menuItem of menu_items) {
+			const { rows: inventoryItems } = await pool.query(
+				`SELECT * FROM inventory_to_menu WHERE menu_id = $1;`,
+				[menuItem.menu_id]
+			);
+
+			for (const inventoryItem of inventoryItems) {
+				await pool.query(
+					`UPDATE inventory
+			 SET quantity = quantity - $1
+			 WHERE inventory_id = $2;`,
+					[
+						inventoryItem.quantity * menuItem.quantity,
+						inventoryItem.inventory_id,
+					]
+				);
+			}
+		}
+
+		res.status(201).json({ order_id });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
 // GET all orders
 orderRouter.get("/", async (req, res) => {
 	try {
